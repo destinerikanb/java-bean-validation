1714d7204db83551ea8bfa7037e58938f5c0dd0f
destinerikanb <destinerikanb@gmail.com>
2022-12-04 19:04:54 +0700

    Finish initial setup

---
+++ src/test/java/com/destinerikanb/javavalidation/JavaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destinerikanb.javavalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
